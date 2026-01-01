38f916b7284634597dd83efe8a01a346280786df
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2025-12-31 16:10:54 -0800

    Modify install.bat for pdfFactory Pro installation
    Updated installation script to download and install pdfFactory Pro instead of pdfFactory. Added license key application for both FinePrint and pdfFactory Pro.

--- install.bat
+++ install.bat
@@ -17,14 +17,14 @@ if not exist "C:\Program Files\7-Zip\7z.exe" (
 echo Downloading FinePrint...
 powershell -command "Invoke-WebRequest -Uri '%REPO%/fp1144.exe' -OutFile '%TEMP_DIR%\fp1144.exe'"
 
-echo Downloading pdfFactory...
-powershell -command "Invoke-WebRequest -Uri '%REPO%/pdf844std.exe' -OutFile '%TEMP_DIR%\pdf844std.exe'"
+echo Downloading pdfFactory Pro...
+powershell -command "Invoke-WebRequest -Uri '%REPO%/pdf844pro.exe' -OutFile '%TEMP_DIR%\pdf844pro.exe'"
 
 echo Extracting FinePrint...
 "C:\Program Files\7-Zip\7z.exe" x "%TEMP_DIR%\fp1144.exe" -o"%TEMP_DIR%\fp" -y >nul
 
-echo Extracting pdfFactory...
-"C:\Program Files\7-Zip\7z.exe" x "%TEMP_DIR%\pdf844std.exe" -o"%TEMP_DIR%\pdf" -y >nul
+echo Extracting pdfFactory Pro...
+"C:\Program Files\7-Zip\7z.exe" x "%TEMP_DIR%\pdf844pro.exe" -o"%TEMP_DIR%\pdf" -y >nul
 
 :: Stop services that might hold file locks
 echo Preparing system...
@@ -36,13 +36,17 @@ net start spooler >nul 2>&1
 echo Installing FinePrint...
 "%TEMP_DIR%\fp\setup-x64.exe" /install /quiet=4109 /force /noini /restartspooler
 
-echo Installing pdfFactory...
+echo Installing pdfFactory Pro...
 "%TEMP_DIR%\pdf\setup-x64.exe" /install /quiet=4109 /force /noini /restartspooler
+
+:: Add license keys
+echo Applying license keys...
+reg add "HKCU\SOFTWARE\FinePrint Software\FinePrint11" /v RegNum /t REG_SZ /d ${fineprintprodkey} /f >nul
+reg add "HKLM\SOFTWARE\FinePrint Software\FinePrint11" /v RegNum /t REG_SZ /d ${fineprintprodkey} /f >nul
+reg add "HKCU\SOFTWARE\FinePrint Software\pdfFactory8" /v RegNum /t REG_SZ /d ${pdfproprodkey} /f >nul
+reg add "HKLM\SOFTWARE\FinePrint Software\pdfFactory8" /v RegNum /t REG_SZ /d ${pdfproprodkey} /f >nul
 
 echo Cleaning up...
 rd /s /q "%TEMP_DIR%"
-echo adding product keys
-:: Add FinePrint license key
-reg add "HKCU\SOFTWARE\FinePrint Software\FinePrint11" /v RegNum /t REG_SZ /d ${prodkey} /f
-reg add "HKLM\SOFTWARE\FinePrint Software\FinePrint11" /v RegNum /t REG_SZ /d ${prodkey} /f
+
 echo Done.
